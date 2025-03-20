7dd2b6793a357bbd69b8c568880be831ca570ef6
leehangchi <leehangchi102@gmail.com>
2025-03-21 09:23:47 +1000

    change ip to 13.236.200.125/ three

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -5,5 +5,6 @@ const axiosInstance = axios.create({
   baseURL: 'http://13.236.200.125:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
-// change ip to 13.236.200.125 two
+// change ip to 13.236.200.125
+// change ip to 13.236.200.125
 export default axiosInstance;
